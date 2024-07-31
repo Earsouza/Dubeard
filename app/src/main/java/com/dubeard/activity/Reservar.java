a70ed65966f9cad48216b0701441460f71d8b67f
Earsouza <eduardo.ar2001@aluno.ifsc.edu.br>
2024-07-31 20:54:49 -0300

    atualizacao 31-07

--- app/src/main/java/com/dubeard/activity/Reservar.java
+++ app/src/main/java/com/dubeard/activity/Reservar.java
@@ -4,9 +4,9 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -16,9 +16,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dubeard.R;
-import com.dubeard.activity.barber.model.Barber;
-import com.dubeard.activity.barber.model.Reserva;
 import com.dubeard.activity.barber.model.Service;
+import com.dubeard.activity.barber.model.Reserva;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.firebase.database.*;
@@ -26,7 +25,6 @@ import com.google.firebase.database.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 import java.util.TimeZone;
 
 public class Reservar extends AppCompatActivity {
@@ -34,10 +32,13 @@ public class Reservar extends AppCompatActivity {
     private Spinner spinnerServico;
     private Button btnDatePicker, btnTimePicker, btReservar, btVoltar, btCancelar;
     private TextView textSelectedDate, textSelectedTime;
-    final String tag = "DATE_TAG";
-    DatabaseReference databaseReference;
-    ArrayAdapter<Service> arrayAdapterServices;
-    ArrayList<Service> arrayListServices = new ArrayList<>();
+    private final String tag = "DATE_TAG";
+    private DatabaseReference servicoReference;
+    private DatabaseReference reservaReference;
+    private ArrayAdapter<Service> arrayAdapterServices;
+    private ArrayList<Service> arrayListServices = new ArrayList<>();
+    private int i = 0;
+    private Service selectedService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,44 +47,70 @@ public class Reservar extends AppCompatActivity {
 
         initComponents();
 
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("servico");
+        servicoReference = FirebaseDatabase.getInstance().getReference().child("servico");
+        reservaReference = FirebaseDatabase.getInstance().getReference().child("reserva");
 
-        arrayAdapterServices = new ArrayAdapter<Service>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayListServices);
+        setupServicoListener();
+        setupDatePicker();
+        setupTimePicker();
+        setupReservaButton();
+        setupCancelButton();
+
+        arrayAdapterServices = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayListServices);
         spinnerServico.setAdapter(arrayAdapterServices);
 
-        databaseReference.addChildEventListener(new ChildEventListener() {
+        // Configurar OnItemSelectedListener para o Spinner
+        spinnerServico.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedService = (Service) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Implementar o que deve acontecer quando nada é selecionado, se necessário
+            }
+        });
+    }
+
+    private void initComponents() {
+        btReservar = findViewById(R.id.btReservar);
+        btVoltar = findViewById(R.id.btvoltar);
+        btnDatePicker = findViewById(R.id.btn_date_picker);
+        btnTimePicker = findViewById(R.id.btn_time_picker);
+        textSelectedTime = findViewById(R.id.text_selected_time);
+        textSelectedDate = findViewById(R.id.text_selected_date);
+        btCancelar = findViewById(R.id.btCancelarReserva);
+        spinnerServico = findViewById(R.id.spinnerServicos);
+    }
+
+    private void setupServicoListener() {
+        servicoReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 Service service = snapshot.getValue(Service.class);
-               arrayListServices.add(
-                        new Service(snapshot.getKey(),
-                                service.getDescricao(),
-                                service.getValor())
-                );
-
+                arrayListServices.add(new Service(snapshot.getKey(), service.getDescricao(), service.getValor()));
                 arrayAdapterServices.notifyDataSetChanged();
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-            }
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
 
             @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-            }
-
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) { }
 
             @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-            }
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
+            public void onCancelled(@NonNull DatabaseError error) { }
         });
+    }
 
+    private void setupDatePicker() {
         MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
         MaterialDatePicker materialDatePicker = builder.build();
+
         btnDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -96,13 +123,16 @@ public class Reservar extends AppCompatActivity {
                 });
             }
         });
+    }
 
+    private void setupTimePicker() {
         btnTimePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar calendar = Calendar.getInstance();
                 int hour = calendar.get(Calendar.HOUR_OF_DAY);
                 int mins = calendar.get(Calendar.MINUTE);
+
                 TimePickerDialog timePickerDialog = new TimePickerDialog(Reservar.this, androidx.appcompat.R.style.Theme_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int horasDia, int minutosDia) {
@@ -118,7 +148,41 @@ public class Reservar extends AppCompatActivity {
                 timePickerDialog.show();
             }
         });
+    }
 
+    private void setupReservaButton() {
+        reservaReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    i = (int) snapshot.getChildrenCount();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle possible errors.
+            }
+        });
+
+        btReservar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Reserva reserva = new Reserva(
+                        textSelectedDate.getText().toString(),
+                        textSelectedTime.getText().toString(),
+                        selectedService.getDescricao()
+                );
+
+                reservaReference.push().setValue(reserva);
+
+                Intent intent = new Intent(getApplicationContext(), MainClient.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setupCancelButton() {
         btCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -129,35 +193,17 @@ public class Reservar extends AppCompatActivity {
 
     private void clearInput() {
         textSelectedDate.setText("");
-        textSelectedDate.setText("");
-    }
-
-    private void initComponents() {
-        btReservar = findViewById(R.id.btReservar);
-        btVoltar = findViewById(R.id.btvoltar);
-        btnDatePicker = findViewById(R.id.btn_date_picker);
-        btnTimePicker = findViewById(R.id.btn_time_picker);
-        textSelectedTime = findViewById(R.id.text_selected_time);
-        textSelectedDate = findViewById(R.id.text_selected_date);
-        btCancelar = findViewById(R.id.btCancelarReserva);
-        spinnerServico = findViewById(R.id.spinnerServicos);
+        textSelectedTime.setText("");
+        spinnerServico.setSelection(0);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
 
-        // Defina a tela de destino que você deseja abrir ao pressionar o botão de voltar
         Intent intent = new Intent(this, MainClient.class);
-
-        // Adicione flags para limpar a pilha de atividades, se desejar
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-
-        // Inicie a nova atividade
         startActivity(intent);
-
-        // Finalize a atividade atual, se desejar
         finish();
     }
-
 }
